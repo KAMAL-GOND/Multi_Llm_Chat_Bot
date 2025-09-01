90632ceb755c1760f40f2fcb4944ddf3501d3087
kamal gond <kamalgond0@gmail.com>
2025-09-01 22:39:21 +0530

    ui fixed

--- app/src/main/java/com/example/multi_llm_chat_bot/SCREEN/InteractionScreen.kt
+++ app/src/main/java/com/example/multi_llm_chat_bot/SCREEN/InteractionScreen.kt
@@ -2,13 +2,19 @@ package com.example.multi_llm_chat_bot.SCREEN
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.BottomAppBar
@@ -19,6 +25,7 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
@@ -55,30 +62,197 @@ fun InteractionScreen(viewModel: chatBotVeiwModel) {
      val models = listOf<String>("deepseek/deepseek-chat-v3-0324:free","deepseek/deepseek-r1:free","z-ai/glm-4.5-air:free","qwen/qwen3-coder:free","google/gemini-2.0-flash-exp:free","microsoft/mai-ds-r1:free","meta-llama/llama-3.3-70b-instruct:free","openai/gpt-oss-20b:free","google/gemma-3-27b-it:free") // Unused
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(currentModel) },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color(0xFFF0F0F0), // A light gray for example
-                    titleContentColor = Color.Black
-                )
-            )
-            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-                models.forEach {
+       modifier = Modifier.imePadding().background(color = Color(9, 9, 9)), // Add imePadding to the Scaffold
+////
+////        topBar = {
+////            TopAppBar(
+////                title = { Text(currentModel) },
+////                colors = TopAppBarDefaults.topAppBarColors(
+////                    containerColor = Color(0xFF494848), // A light gray for example
+////                    titleContentColor = Color.Black
+////                )
+////            )
+//            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+//                models.forEach {
+//                    DropdownMenuItem(
+//                        text = { Text(it) },
+//                        onClick = {
+//                            currentModel = it.toString()
+//                            expanded = false
+//                        }
+//                    )
+//                }
+//            }
+////        },
+////        bottomBar = {
+////            Card(
+////                modifier = Modifier
+////                    .fillMaxWidth()
+////                    .padding(horizontal = 8.dp, vertical = 4.dp), // Add some padding
+////                shape = RoundedCornerShape(24.dp), // Rounded corners for the card
+////                elevation = CardDefaults.cardElevation(4.dp)
+////            ) {
+////                Box(
+////                    modifier = Modifier
+////                        .fillMaxWidth()
+////                        .padding(horizontal = 8.dp, vertical = 4.dp), // Padding inside the card
+////                    contentAlignment = Alignment.Center
+////                ) {
+////                    OutlinedTextField(
+////                        modifier = Modifier.fillMaxWidth(),
+////                        value = question,
+////                        onValueChange = { question = it },
+////                        placeholder = { Text(text = "Type a question...") },
+////                        trailingIcon = {
+////                            IconButton(onClick = {
+////                                // TODO: Implement send action
+////                                // viewModel.sendMessage(question, currentModel)
+////                                viewModel.getAnswer(currentModel, question)
+////                                ShownQuestion = question
+////                                question = "" // Clear input after sending
+////                            }) {
+////                                Icon(
+////                                    imageVector = Icons.Default.Send,
+////                                    contentDescription = "Send"
+////                                )
+////                            }
+////                        },
+////                        shape = RoundedCornerShape(24.dp) // Match card shape
+////                    )
+////                }
+////            }
+////        },
+//        floatingActionButton = {
+//            FloatingActionButton(onClick = {
+//                expanded = !expanded
+//
+//            }){Icon(imageVector = Icons.Default.KeyboardArrowUp, contentDescription = "Send")}
+//        }
+   )
+
+    { innerPadding ->
+        Column(modifier = Modifier.fillMaxSize().padding(innerPadding).background(Color(10,10,10))) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth().fillMaxWidth(0.13f)
+                    .padding(horizontal = 8.dp, vertical = 4.dp), // Add some padding
+                //shape = RoundedCornerShape(24.dp), // Rounded corners for the card
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+               models.forEach {
                     DropdownMenuItem(
-                        text = { Text(it) },
+                       text = { Text(it) },
                         onClick = {
                             currentModel = it.toString()
                             expanded = false
                         }
                     )
+               }
+            }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 4.dp), // Padding inside the card
+                    contentAlignment = Alignment.Center
+                ) {
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = currentModel.toString(),
+                        readOnly = true,
+                        onValueChange = {  },
+                       // readOnly = true,
+                        //placeholder = { Text(text = "Type a question...") },
+                        trailingIcon = {
+                            IconButton(onClick = {
+                                // TODO: Implement send action
+                                // viewModel.sendMessage(question, currentModel)
+//                                viewModel.getAnswer(currentModel, question)
+//                                ShownQuestion = question
+//                                question = "" // Clear input after sending
+                                expanded = !expanded
+
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowDown,
+                                    contentDescription = "Send"
+                                )
+                            }
+                       },
+                        //shape = RoundedCornerShape(24.dp) // Match card shape
+                    )
                 }
             }
-        },
-        bottomBar = {
+            Box(modifier = Modifier.fillMaxHeight(0.85f).padding(5.dp).background(Color(
+                35,
+                35,
+                42,
+                255
+            ),
+
+                ), contentAlignment = Alignment.TopCenter
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+
+                        .padding(5.dp) // Apply padding from Scaffold
+                        .padding(horizontal = 16.dp, vertical = 8.dp) // Additional padding for content
+                ) {
+                    item {
+                        Text(
+                            text = "Question : $ShownQuestion",
+                            modifier = Modifier.background(Color.LightGray),
+                            color = Color.White,
+                            fontSize = 20.sp,
+                            //fontSize = 10.dp,
+                            //fontStyle = FontStyle.Normal,
+
+                            textAlign = TextAlign.Right
+                        )
+                    }
+                    item { Spacer(modifier = Modifier.height(4.dp)) }
+                    item { HorizontalDivider(modifier = Modifier.height(3.dp),color = Color.White) }
+                    item { Spacer(modifier = Modifier.height(4.dp)) }
+
+                    when {
+                        state.isLoading -> {
+                            item {
+                                Box(modifier = Modifier.fillMaxSize(),contentAlignment = Alignment.Center) {
+                                    CircularProgressIndicator()
+                                }
+                            }
+                        }
+
+                        state.response != null -> {
+                            item {
+                                Text(text = state.response!!.choices?.get(0)!!.message!!.content!!.toString(),
+                                    // text = "Question : $ShownQuestion",
+                                    modifier = Modifier,
+                                    color = Color.White,
+                                    fontSize = 20.sp,
+                                    //fontSize = 10.dp,
+                                    //fontStyle = FontStyle.Normal,
+
+                                    textAlign = TextAlign.Left)
+                            }
+                        }
+
+                        state.error != null -> {
+                            item {
+                                Text(text = state.error!!, color=Color(255, 71, 71, 255))
+                            }
+                        }
+
+                    }
+
+                    // items(state.messages) { message ->
+                    //    Text(text = message.content)
+                    // }
+                }
+            }
             Card(
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxWidth().fillMaxWidth(0.3f)
                     .padding(horizontal = 8.dp, vertical = 4.dp), // Add some padding
                 shape = RoundedCornerShape(24.dp), // Rounded corners for the card
                 elevation = CardDefaults.cardElevation(4.dp)
@@ -112,110 +286,44 @@ fun InteractionScreen(viewModel: chatBotVeiwModel) {
                     )
                 }
             }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                expanded = !expanded
 
-            }){Icon(imageVector = Icons.Default.KeyboardArrowUp, contentDescription = "Send")}
         }
-    ) { innerPadding ->
-        Box(modifier = Modifier.fillMaxHeight().fillMaxWidth().padding(innerPadding).background(Color(
-            35,
-            35,
-            42,
-            255
-        )
-        )) {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding) // Apply padding from Scaffold
-                .padding(horizontal = 16.dp, vertical = 8.dp) // Additional padding for content
-        ) {
-            item {
-                Text(
-                    text = "Question : $ShownQuestion",
-                    modifier = Modifier,
-                    color = Color.White,
-                    fontSize = 20.sp,
-                    //fontSize = 10.dp,
-                    //fontStyle = FontStyle.Normal,
 
-                    textAlign = TextAlign.Right
-                )
-            }
-            when {
-                state.isLoading -> {
-                    item {
-                        Box(modifier = Modifier.fillMaxSize(),contentAlignment = Alignment.Center) {
-                            CircularProgressIndicator()
-                        }
-                    }
-                }
+//        Card(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(horizontal = 8.dp, vertical = 4.dp), // Add some padding
+//            shape = RoundedCornerShape(24.dp), // Rounded corners for the card
+//            elevation = CardDefaults.cardElevation(4.dp)
+//        ) {
+//            Box(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .padding(horizontal = 8.dp, vertical = 4.dp), // Padding inside the card
+//                contentAlignment = Alignment.Center
+//            ) {
+//                OutlinedTextField(
+//                    modifier = Modifier.fillMaxWidth(),
+//                    value = question,
+//                    onValueChange = { question = it },
+//                    placeholder = { Text(text = "Type a question...") },
+//                    trailingIcon = {
+//                        IconButton(onClick = {
+//                            // TODO: Implement send action
+//                            // viewModel.sendMessage(question, currentModel)
+//                            viewModel.getAnswer(currentModel,question)
+//                            ShownQuestion=question
+//                            question = "" // Clear input after sending
+//                        }) {
+//                            Icon(
+//                                imageVector = Icons.Default.Send,
+//                                contentDescription = "Send"
+//                            )
+//                        }
+//                    },
+//                    shape = RoundedCornerShape(24.dp) // Match card shape
+//                )
+//            }
+//        }
+    }}
 
-                state.response != null -> {
-                    item {
-                        Text(text = state.response!!.choices?.get(0)!!.message!!.content!!.toString(),
-                           // text = "Question : $ShownQuestion",
-                            modifier = Modifier,
-                            color = Color.White,
-                            fontSize = 20.sp,
-                            //fontSize = 10.dp,
-                            //fontStyle = FontStyle.Normal,
-
-                            textAlign = TextAlign.Left)
-                    }
-                }
-
-                state.error != null -> {
-                    item {
-                        Text(text = state.error!!)
-                    }
-                }
-
-            }
-
-            // items(state.messages) { message ->
-            //    Text(text = message.content)
-            // }
-        }
-    }
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 8.dp, vertical = 4.dp), // Add some padding
-            shape = RoundedCornerShape(24.dp), // Rounded corners for the card
-            elevation = CardDefaults.cardElevation(4.dp)
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 8.dp, vertical = 4.dp), // Padding inside the card
-                contentAlignment = Alignment.Center
-            ) {
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    value = question,
-                    onValueChange = { question = it },
-                    placeholder = { Text(text = "Type a question...") },
-                    trailingIcon = {
-                        IconButton(onClick = {
-                            // TODO: Implement send action
-                            // viewModel.sendMessage(question, currentModel)
-                            viewModel.getAnswer(currentModel,question)
-                            ShownQuestion=question
-                            question = "" // Clear input after sending
-                        }) {
-                            Icon(
-                                imageVector = Icons.Default.Send,
-                                contentDescription = "Send"
-                            )
-                        }
-                    },
-                    shape = RoundedCornerShape(24.dp) // Match card shape
-                )
-            }
-        }
-    }
-}
